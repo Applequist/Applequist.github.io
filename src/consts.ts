669b0e80fd8cf7251f63d5b29fd4d9647828e1e7
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2026-03-19 23:42:48 +0100

    Content: Add better meta information in pages

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'brieuc.desoutter';
-export const SITE_DESCRIPTION = 'Hi! Welcome to my little corner of the Internet!';
+export const SITE_DESCRIPTION = 'Personal site and blog of Brieuc Desoutter, a senior software engineer writing about Rust, Haskell and other topics';
 export const GH_URL = 'https://github.com/Applequist';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/bdesoutter';
